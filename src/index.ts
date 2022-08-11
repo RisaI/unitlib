5125ffef7469b48f93d954b486721d2e083dde75
Richard Ivanek <richard.ivanek@gmail.com>
2022-08-11 09:23:29 +0200

    Package.json export field

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 
 export { Unit } from './Unit';
 export { UnitSystem } from './UnitSystem';
-export * as definitions from './definitions';
-export * as utils from './utils';
 
 export type {
     ValueWithUnit,
