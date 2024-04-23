60e645e4d1a66602fafad169a0a4bbb84085f836
m93a <m93a.cz@gmail.com>
2024-04-23 13:09:34 +0200

    fix: export isUnit && isQuantity as a value, not as a type

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export * from './Unit.ts';
 export * from './UnitSystem.ts';
 export * from './Quantity.ts';
 
-export type * from './types.ts';
+export * from './types.ts';
 
 export { formatFloat, roundFloat } from './float.ts';
